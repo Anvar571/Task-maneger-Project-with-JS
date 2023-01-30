8d427ffb967565799479ee4b2fd89729da125b72
Anvar571 <abdurashidovanvar52@gmail.com>
2023-01-31 01:14:24 +0500

    Project finished

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// setTimeout(() => {
+//   alert(
+//     `Saytdan foydalanish Agar biror taskni o'chirmoqchi bo'lsangiz o'sh taskni ustiga ikki marta bosing!`
+//   );
+// }, 3000);
+
+import Kanban from "./view/Kanban.js";
+
+new Kanban(document.querySelector(".kanban"));
---
+++ js/view/Kanban.js
@@ -0,0 +1,32 @@
+import Column from "./Columns.js";
+
+export default class Kanban {
+    constructor(root) {
+        this.root = root;
+
+
+        Kanban.columns().forEach(column => {
+            // todo: create an instance Column class
+            const columnView = new Column(column.id, column.title);
+
+            this.root.appendChild(columnView.elements.root);
+        })
+    }
+
+    static columns(){
+        return [
+            {
+                id: 1,
+                title: "Not started",
+            },
+            {
+                id: 2,
+                title: "In Progress",
+            },
+            {
+                id: 3,
+                title: "Completed",
+            },
+        ]
+    }
+}
